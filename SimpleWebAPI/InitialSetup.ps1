64eb535f0dde7166fb1129b31d42f37a6abe72f1
Laz <lazaropadron@gmail.com>
2020-11-16 12:30:43 -0600

    Update initial setup script

--- SimpleWebAPI/InitialSetup.ps1
+++ SimpleWebAPI/InitialSetup.ps1
@@ -1,4 +1,10 @@
+# Install dotnet-ef
+dotnet tool install --global dotnet-ef
+
+# Clean up in case rerun
 dotnet ef migrations remove
+
+# Create DB migrations for EF
 dotnet ef migrations add InitialCreate
 dotnet ef database update
 
